dd3d4122975c894b1e27f6aed4015cb716ea0113
Josh Goldberg <me@joshuakgoldberg.com>
2020-08-16 09:24:52 -0400

    Shared video buttons

--- src/pages/room/Scene/VideoArea/VideoButtons.tsx
+++ src/pages/room/Scene/VideoArea/VideoButtons.tsx
@@ -1,38 +1,24 @@
-import { Entity as AREntity } from "aframe-react";
 import React from "react";
 
 import { useRoomContext } from "../../RoomContext";
-
-const mouseEnter =
-  "property: scale; to: 0.27 0.27 1; dur: 300; startEvents: mouseenter";
-
-const mouseLeave =
-  "property: scale; to: 0.2 0.2 1; dur: 300; startEvents: mouseleave";
+import { VideoButton } from "./VideoButton";
 
 export const VideoButtons: React.FC = () => {
   const { currentSongIndex, songs, playing, volume } = useRoomContext();
 
   return (
     <>
-      <AREntity
-        animation__mouseenter={mouseEnter}
-        animation__mouseleave={mouseLeave}
+      <VideoButton
         id="play-pause-button"
         events={{
           click: () => playing.set(!playing.get()),
         }}
-        play-pause
         position="-0.4 1 -1"
-        primitive="a-image"
-        scale="0.2 0.2 1"
         src={playing.get() ? "#pause" : "#play"}
       />
 
-      <AREntity
-        animation__mouseenter={mouseEnter}
-        animation__mouseleave={mouseLeave}
+      <VideoButton
         id="next-button"
-        next
         events={{
           click: () => {
             currentSongIndex.set(
@@ -41,14 +27,10 @@ export const VideoButtons: React.FC = () => {
           },
         }}
         position="-0.1 1 -1"
-        primitive="a-image"
-        scale="0.2 0.2 1"
         src="#next"
       />
 
-      <AREntity
-        animation__mouseenter={mouseEnter}
-        animation__mouseleave={mouseLeave}
+      <VideoButton
         id="previous-button"
         events={{
           click: () => {
@@ -56,38 +38,27 @@ export const VideoButtons: React.FC = () => {
           },
         }}
         position="-0.7 1 -1"
-        primitive="a-image"
-        previous
-        scale="0.2 0.2 1"
         src="#previous"
       />
 
-      <AREntity
-        animation__mouseenter={mouseEnter}
-        animation__mouseleave={mouseLeave}
+      <VideoButton
         id="volume-low-button"
         events={{
           click: () => volume.set(Math.max(volume.get() - 0.25, 0)),
         }}
-        primitive="a-image"
+        opacity={volume.get() === 0 ? 0.5 : 1}
         position="0.4 1 -1"
-        scale="0.2 0.2 1"
         src="#volume-low"
-        volume-low
       />
 
-      <AREntity
-        animation__mouseenter={mouseEnter}
-        animation__mouseleave={mouseLeave}
+      <VideoButton
         id="volume-high-button"
         events={{
           click: () => volume.set(Math.min(volume.get() + 0.25, 1)),
         }}
-        primitive="a-image"
+        opacity={volume.get() === 1 ? 0.5 : 1}
         position="0.7 1 -1"
-        scale="0.2 0.2 1"
         src="#volume-high"
-        volume-high
       />
     </>
   );
