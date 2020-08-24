cf03df9612c2e3435f088ffc163364ef86874956
Josh Goldberg <me@joshuakgoldberg.com>
2020-08-23 21:19:24 -0400

    Disable unnecessary lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   rules: {
     "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/restrict-template-expressions": "off",
   },
 };
